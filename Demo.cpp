d5a78863bfd4206d90b2db0fba5a7351b564c46a
SonK4m3 <nguyenhongsont1lhp@gmail.com>
2021-12-01 13:13:18 +0700

    change Demo

--- Demo.cpp
+++ Demo.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
-const int MAX = 20;
+const int MAX = 9;
+bool display_STOP, game_STOP;
 
 struct Banco {
 	int a[MAX][MAX];
@@ -14,6 +15,8 @@ struct Player {
 }player1, player2;
 
 void inbanco();
+bool isInt(string s);
+int Str_Int(string s);
 
 // khoi tao nguoi choi
 void khoitao_player(Player &p1, Player &p2){
@@ -41,7 +44,13 @@ void khoitao_player(Player &p1, Player &p2){
 // khoi tao ban co
 void khoitao_banco(){
 	cout << "Nhap kich thuoc ban co:\n";
-	cin >> banco.n;
+	while(cin >> banco.n){
+		if(banco.n < 1 || banco.n > MAX)
+			cout << "Nhap lai kich thuoc ban co ...\n";
+		else 
+			break;
+	}
+	
 	for(int i = 0; i < banco.n; i ++){
 		for(int j = 0; j < banco.n; j++)
 			banco.a[i][j] = -1;
@@ -60,54 +69,104 @@ void khoitao(){
 
 // in ban co
 void inbanco(){
-	for(int i = 0; i <= banco.n; i ++){
-		if(i == 0)	cout << "  ";
-		else cout << " " << i << " ";
-	}
-	cout << endl;
-	for(int i = 0; i < banco.n; i ++){
-		cout << i + 1<< " ";
-		for(int j = 0; j < banco.n; j ++){
-			if(banco.a[i][j] == -1)
-				cout << " - ";	
-			else if(banco.a[i][j] == 1)
-				cout << " X ";
-			else if(banco.a[i][j] == 0)
-				cout << " O ";
+	if(!display_STOP){
+		for(int i = 0; i <= banco.n; i ++){
+			if(i == 0)	cout << "  ";
+			else cout << " " << i << " ";
 		}
+		
 		cout << endl;
+		
+		for(int i = 0; i < banco.n; i ++){
+			cout << i + 1<< " ";
+			for(int j = 0; j < banco.n; j ++){
+				if(banco.a[i][j] == -1)
+					cout << " - ";	
+				else if(banco.a[i][j] == 1)
+					cout << " X ";
+				else if(banco.a[i][j] == 0)
+					cout << " O ";
+			}
+			cout << endl;
+		}
 	}
 }
 
-// in ra man hinh
+// in thong bao 
 void inthongbao(){
-	cout << "Nhap nuoc co tiep theo\n";
-	cout << "Nhap -1 de thoat.\n";
+	if(!display_STOP){
+		cout << "Nhap nuoc co tiep theo\n";
+		cout << "Nhap exit de thoat.\n";
+	}
+} 
+
+// nhap input
+void nhap(Player &p1, Player &p2, string mes = "Player 1 turn: \n"){
+	if(p1.in_turn == 'Y'){
+		cout << mes;
+			
+		string input_player;
+			
+		scanf("\n");
+		getline(cin, input_player);
+		
+		int x = 0, y = 0;
+		char ch;
+		
+		vector<string> v;
+		stringstream arr(input_player);
+		
+		while(!arr.eof()){
+			string tmp;
+			arr >> tmp;
+			v.push_back(tmp);
+		}
+		
+		if(v.size() == 3 && isInt(v[0]) && isInt(v[1]) && v[2].size() == 1){
+			x = Str_Int(v[0]);
+			y = Str_Int(v[1]);
+			ch = v[2][0];
+			if(x >= 1 && x <= banco.n && y >= 1 && y <= banco.n && v[2][0] == p1.C && banco.a[x - 1][y - 1] == -1){
+				if(ch == 'O')
+					banco.a[x - 1][y - 1] = 0;
+				else if(ch == 'X')
+					banco.a[x - 1][y - 1] = 1;
+				
+				p1.in_turn = 'N';
+				p2.in_turn = 'Y';
+				if(display_STOP)	display_STOP = false;
+			}
+			else if(banco.a[x - 1][y - 1] != -1){
+				cout << "O da duoc danh, Xin nhap lai ...\n";
+				display_STOP = true;
+			}
+			else{
+				cout << "Sai du lieu, Xin nhap lai ...\n";
+				display_STOP = true;
+			}
+		}
+		else if(v.size() == 1 && v[0] == "exit"){
+			cout << "exit game ...\n";
+			display_STOP = true;
+			game_STOP = true;
+		}
+		else{
+			cout << "Sai du lieu, Xin nhap lai ...\n";
+			display_STOP = true;
+		}
+	}
+	else nhap(p2, p1, "Player 2 turn: \n");
 }
 
+// in man hinh
 void Display(){
-	while(true){
-		inthongbao();
-		
-		// nhap input tam thoi. 
-		
-		int x, y;
-		char c;
-		cin >> x >> y >> c;
-		if(x == -1){
-			cout << "Exit...";
-			break;
-		}
-		if(x >= 0 && x <= banco.n && y >= 0 && y <= banco.n && (c == 'X' || c == 'O') && banco.a[x - 1][y - 1] == -1){
-			if(c == 'X')
-				banco.a[x - 1][y - 1] = 1;
-			else
-				banco.a[x - 1][y - 1] = 0;
-		}
-		else
-			cout << "Moi nhap lai...\n";
+	
+	game_STOP = false;
+	display_STOP = false;
+	
+	while(!game_STOP){
 				
-		//
+		nhap(player1, player2);
 			
 		inbanco();
 	}	
@@ -118,4 +177,18 @@ int main(){
 	khoitao();
 	
 	Display();
+}
+
+bool isInt(string s){
+	for(int i = 0; i < s.size(); i++)
+		if(s[i] < '0' && s[i] > '9')
+			return false;
+	return true;
+}
+
+int Str_Int(string s){
+	int res = 0;
+	for(int i = 0; i < s.size(); i++)
+		res = res * 10 + s[i] - '0';
+	return res;
 }
